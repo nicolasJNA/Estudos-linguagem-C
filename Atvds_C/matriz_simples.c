cf8ba92bd285e2fe50e3e013c55db6b56b0a6c44
Nicolas Justo <nicolasJusto@gmail>
2025-09-27 00:26:28 -0300

    atvd simples

---
+++ Atvds_C/matriz_simples.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+    Foram digitadas 3 linhas, cada linha contem o nome e a nota de um alunoo.
+    Escreva um programa que leia essas informações e monte uma tabela onde a 
+    primeira coluna é o nome e a segunda coluna é a nota
+*/
+int main() {
+    char nome[3][30];
+    float notas[3];
+
+    for (int i = 0; i < 3; i++){
+        printf("Digite o nome e a nota do estudante: ");
+        scanf("%s%f", &nome[i], &notas[i]);
+    }
+     for (int i = 0; i < 3; i++){
+        printf("%s  %2.f\n",nome[i], notas[i]);    
+    }
+    return 0;
+}
